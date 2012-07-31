46d97c44b0ddb824a2ee8949bb37b19bc49314bc
Volodymyr_Dvornyk <Volodymyr_Dvornyk@EPUAKYIW0070.kyiv.epam.com>
2012-07-31 16:26:25 +0300

    add unit test for MinCycles project

--- com-epam-test-lcs/src/main/java/com/epam/test/mincycles/CyclesUtils.java
+++ com-epam-test-lcs/src/main/java/com/epam/test/mincycles/CyclesUtils.java
@@ -2,12 +2,17 @@ package com.epam.test.mincycles;
 
 public class CyclesUtils {
 	public static String getCyclesString(String str) {
+		str = str.trim();
+		if (str.equals("")) {
+			return null;
+		}
+
 		int len = str.length();
 		for (int i = 2; i <= len / 2; i++) {
 			String key = str.substring(len - i);
 			String doubleKey = key + key;
 			if (str.substring(len - i - key.length()).equals(doubleKey)) {
-				return key;
+				return key.trim();
 			}
 		}
 		return null;
