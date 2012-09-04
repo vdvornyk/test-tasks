659bded723f775755280952e22a0546a30ff778d
Volodymyr_Dvornyk <Volodymyr_Dvornyk@EPUAKYIW0070.kyiv.epam.com>
2012-09-04 18:01:21 +0300

    template created more

---
+++ com-epam-test-jcin/src/main/java/com/epam/test/jcip/ch4/UnsafeSequence.java
@@ -0,0 +1,18 @@
+package com.epam.test.jcip.ch4;
+
+/**
+ * UnsafeSequence
+ * 
+ * @author Brian Goetz and Tim Peierls
+ */
+
+public class UnsafeSequence {
+	private int value;
+
+	/**
+	 * Returns a unique value.
+	 */
+	public int getNext() {
+		return value++;
+	}
+}
