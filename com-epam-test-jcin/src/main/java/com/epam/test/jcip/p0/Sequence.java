62f121d120a378292425b678425cedda64013cfb
Volodymyr_Dvornyk <Volodymyr_Dvornyk@EPUAKYIW0070.kyiv.epam.com>
2012-09-07 12:45:57 +0300

    ch2 completed

---
+++ com-epam-test-jcin/src/main/java/com/epam/test/jcip/p0/Sequence.java
@@ -0,0 +1,20 @@
+package com.epam.test.jcip.p0;
+
+import net.jcip.annotations.GuardedBy;
+import net.jcip.annotations.ThreadSafe;
+
+/**
+ * Sequence
+ * 
+ * @author Brian Goetz and Tim Peierls
+ */
+
+@ThreadSafe
+public class Sequence {
+	@GuardedBy("this")
+	private int nextValue;
+
+	public synchronized int getNext() {
+		return nextValue++;
+	}
+}
